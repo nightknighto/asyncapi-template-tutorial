67a0636a8e783bbdad4483a71ce7f91bd79a4d2f
NightKnight <Ahmedatwa866@yahoo.com>
2025-04-04 17:25:31 +0200

    Implement MQTT client for temperature service and add test script

--- test/project/client.py
+++ test/project/client.py
@@ -1,1 +1,16 @@
-Temperature Service
+import paho.mqtt.client as mqtt
+mqttBroker = "test.mosquitto.org"
+
+class TemperatureServiceClient:
+    def __init__(self):
+      # 3 
+        self.client = mqtt.Client()
+      # 4
+        self.client.connect(mqttBroker)
+
+
+    def sendTemperatureChange(self, id):
+      # 5
+        topic = "temperature/changed"
+      # 6 
+        self.client.publish(topic, id)
