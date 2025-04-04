05a16ed39af9d98802963583353fc6f61e692687
NightKnight <Ahmedatwa866@yahoo.com>
2025-04-04 17:42:34 +0200

    Refactor index.js to enhance Python client generation with improved formatting and dynamic class naming

--- template/index.js
+++ template/index.js
@@ -1,19 +1,16 @@
-import { File } from "@asyncapi/generator-react-sdk";
+import { File, Text } from "@asyncapi/generator-react-sdk";
 
 export default function ({ asyncapi, params }) {
-    return <File name="client.py">{`
-import paho.mqtt.client as mqtt
-
-mqttBroker = "${asyncapi.servers().get(params.server).url()}"
-
-class TemperatureServiceClient:
-    def __init__(self):
-        self.client = mqtt.Client()
-        self.client.connect(mqttBroker)
-
-
-    def sendTemperatureChange(self, id):
-        topic = "temperature/changed"
-        self.client.publish(topic, id)
-    `}</File>
+    return (
+        <File name="client.py">
+            <Text newLines={2}>import paho.mqtt.client as mqtt</Text>
+            <Text newLines={2}>mqttBroker = "{asyncapi.servers().get(params.server).url()}"</Text>
+            <Text newLines={2}>class {asyncapi.info().title().replaceAll(' ', '')}Client:</Text>
+            <Text indent={2} newLines={2}>
+                {`def __init__(self):
+                    self.client = mqtt.Client()
+                    self.client.connect(mqttBroker)`}
+            </Text>
+        </File>
+    )
 }
